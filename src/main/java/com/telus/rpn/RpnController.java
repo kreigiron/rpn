474530a654745cd6b72752bb445e74412410a93c
Erik Giron <erik.giron@xoom.com>
2018-08-24 14:25:46 -0600

    Add history

--- src/main/java/com/telus/rpn/RpnController.java
+++ src/main/java/com/telus/rpn/RpnController.java
@@ -4,6 +4,7 @@ import com.telus.rpn.calculator.Calculator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,8 +18,16 @@ public class RpnController {
         return "Hello, please inject a valid Calculator in order to calculate reverse polish notation :)";
     }
 
-    @RequestMapping("/calculate/{expression}")
-    public String calculate(@PathVariable("expression") final String expression) {
+    @RequestMapping("/calculate")
+    public String calculate(@RequestParam("expression") final String expression) {
         return calculator.calculate(expression);
+    }
+
+    @RequestMapping("/history")
+    public String[] history(@RequestParam(value = "order-by", required = false) final String orderBy) {
+        if(orderBy == null || "latest".equals(orderBy)) {
+            return new String[] {"This", "should", "return", "the", "array", "sorted", "by", "latest"};
+        }
+        return new String[] {"This", "should", "return", "the", "array", "sorted", "alphabetically"};
     }
 }
